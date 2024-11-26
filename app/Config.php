1931a80dbada11b816e53b22f24426c0fdbc207d
Eduardo Martínez Vázquez <martinez.vazquez.eduardo@iescamas.es>
2024-11-26 13:54:17 +0100

    añadidos cambios integrante 3 (Eduardo)

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos62024";
+     static public $mvc_bd_usuario  = "mycarlosme7f";
+     static public $mvc_bd_clave    = "Q79lpuU6";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
